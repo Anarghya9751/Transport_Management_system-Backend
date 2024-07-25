a24f0804c6dd6e87b53ae26240882bd7e47736e0
kurimillaVinod <kurimillavinod@gmail.com>
2024-07-25 17:50:30 +0530

    Implemented Token Authentication using jwt

--- ets_prototype/auth_app/views.py
+++ ets_prototype/auth_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import CustomUser
+from .serializers import UserSerializer
+#from .permissions import IsAdmin, IsEmployee,IsDriver,Isvendor,IsCompany,IsCommander
+
+class UserListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        users = CustomUser.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
